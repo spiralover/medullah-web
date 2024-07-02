a1abd5f3fd076a38a011a3a84beb9b00d6deabd3
ahmard <ahmard.mu@gmail.com>
2024-07-02 13:50:14 +0100

    feat(time): added serde serializer

--- src/helpers/time.rs
+++ src/helpers/time.rs
@@ -1,7 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use chrono::{Local, NaiveDateTime, TimeDelta};
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serializer};
 
 pub fn now_plus_seconds(sec: i64) -> NaiveDateTime {
     (Local::now() + TimeDelta::try_seconds(sec).unwrap()).naive_local()
@@ -22,11 +22,18 @@ pub fn current_timestamp() -> u64 {
         .as_secs()
 }
 
-#[allow(dead_code)]
-pub fn parse_naive_datetime<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
+pub fn serde_de_datetime<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
 where
     D: Deserializer<'de>,
 {
     let s: &str = Deserialize::deserialize(deserializer)?;
     NaiveDateTime::parse_from_str(s, "%Y-%m-%d %H:%M:%S").map_err(serde::de::Error::custom)
+}
+
+pub fn serde_se_datetime<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let formatted_date = date.format("%Y-%m-%d %H:%M:%S").to_string();
+    serializer.serialize_str(&formatted_date)
 }
