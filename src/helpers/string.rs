2f9560e85d442df7636657965c6f7f0b41adf0fc
ahmard <ahmard.mu@gmail.com>
2024-08-12 00:02:19 +0100

    feat(string): str-based uuid generation helper

--- src/helpers/string.rs
+++ src/helpers/string.rs
@@ -1,5 +1,16 @@
-#[cfg(feature = "feat-regex")]
-pub fn is_username_valid(name: String) -> Box<fancy_regex::Result<bool>> {
-    let regex = fancy_regex::Regex::new(r"^[a-z][a-z\d\.]{0,37}$").unwrap();
-    Box::new(regex.is_match(name.as_str()))
+use uuid::Uuid;
+
+pub struct Str;
+
+impl Str {
+    #[cfg(feature = "feat-regex")]
+    pub fn is_username_valid(name: String) -> Box<fancy_regex::Result<bool>> {
+        let regex = fancy_regex::Regex::new(r"^[a-z][a-z\d\.]{0,37}$").unwrap();
+        Box::new(regex.is_match(name.as_str()))
+    }
+
+    /// Generate uuid v4 based id with dashes(-) removed
+    pub fn uuid() -> String {
+        Uuid::new_v4().to_string().replace("-", "")
+    }
 }
