540e07eddd0547c7a5233f19d2ef15809b91cb38
GitHub Action <action@github.com>
2024-09-02 08:35:19 +0000

    style(cargo-fmt): apply changes

--- src/http/middlewares/mod.rs
+++ src/http/middlewares/mod.rs
@@ -1,16 +1,16 @@
+use crate::http::middlewares::executor::MiddlewareExecutor;
+use crate::results::AppResult;
+use ntex::web::{HttpRequest, WebResponse};
 use std::future::Future;
 use std::pin::Pin;
-use ntex::web::{HttpRequest, WebResponse};
-use crate::http::middlewares::executor::{MiddlewareExecutor};
-use crate::results::AppResult;
 
 mod executor;
 
 pub type BeforeMiddlewareHandler =
-fn(HttpRequest) -> Pin<Box<dyn Future<Output = AppResult<HttpRequest>>>>;
+    fn(HttpRequest) -> Pin<Box<dyn Future<Output = AppResult<HttpRequest>>>>;
 
 pub type AfterMiddlewareHandler =
-fn(WebResponse) -> Pin<Box<dyn Future<Output = AppResult<WebResponse>>>>;
+    fn(WebResponse) -> Pin<Box<dyn Future<Output = AppResult<WebResponse>>>>;
 
 #[derive(Clone)]
 pub enum Middleware {
