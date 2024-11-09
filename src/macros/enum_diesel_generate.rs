4230fdb220d0e98bc269bc7d2efd068016dadf32
ahmard <ahmard.mu@gmail.com>
2024-11-09 15:02:53 +0100

    refactor(enum): generation macro should be diesel-dependant

--- src/macros/enum_diesel_generate.rs
+++ src/macros/enum_diesel_generate.rs
@@ -3,9 +3,8 @@ macro_rules! generate_enum {
     // The macro takes the enum name and its variants as input
     ($enum_name:ident { $($variant_name:ident),* }) => {
         // Conditional derive attributes for the database feature
-        #[cfg_attr(feature = "feat-database", derive(diesel::AsExpression, diesel::FromSqlRow))]
-        #[cfg_attr(feature = "feat-database", diesel(sql_type = diesel::sql_types::Text))]
-        #[derive(strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
+        #[derive(diesel::AsExpression, diesel::FromSqlRow, strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
+        #[diesel(sql_type = diesel::sql_types::Text)]
         #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
         pub enum $enum_name {
             $(
@@ -16,8 +15,7 @@ macro_rules! generate_enum {
         // Implement common traits for the enum
         medullah_web::impl_enum_common_traits!($enum_name);
 
-        // Conditional trait implementation for Diesel if the 'database' feature is enabled
-        #[cfg(feature = "feat-database")]
+        // Implement traits required for Diesel usage
         medullah_web::impl_enum_diesel_traits!($enum_name);
     };
 }
