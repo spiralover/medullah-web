3d1ed664be21e687d8da967bc084301db362c179
ahmard <ahmard.mu@gmail.com>
2024-11-09 19:57:28 +0100

    refactor(enum): separate diesel enum generator with regular enum generator

--- src/macros/enum_generate.rs
+++ src/macros/enum_generate.rs
@@ -1,10 +1,9 @@
 #[macro_export]
-macro_rules! generate_diesel_enum {
+macro_rules! generate_enum {
     // The macro takes the enum name and its variants as input
     ($enum_name:ident { $($variant_name:ident),* }) => {
         // Conditional derive attributes for the database feature
-        #[derive(diesel::AsExpression, diesel::FromSqlRow, strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
-        #[diesel(sql_type = diesel::sql_types::Text)]
+        #[derive(strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
         #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
         pub enum $enum_name {
             $(
@@ -14,8 +13,5 @@ macro_rules! generate_diesel_enum {
 
         // Implement common traits for the enum
         medullah_web::impl_enum_common_traits!($enum_name);
-
-        // Implement traits required for Diesel usage
-        medullah_web::impl_enum_diesel_traits!($enum_name);
     };
 }
