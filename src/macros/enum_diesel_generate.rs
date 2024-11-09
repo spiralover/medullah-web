7470dc893d383d4ad0937dc8e9fe4f2673f78539
ahmard <ahmard.mu@gmail.com>
2024-11-09 23:43:15 +0100

    refactor(enum): separate diesel enum generator with feature with which diesel attributes will be derived when it's enabled

--- src/macros/enum_diesel_generate.rs
+++ src/macros/enum_diesel_generate.rs
@@ -19,3 +19,27 @@ macro_rules! generate_diesel_enum {
         medullah_web::impl_enum_diesel_traits!($enum_name);
     };
 }
+
+#[macro_export]
+macro_rules! generate_diesel_enum_with_optional_features {
+    // The macro accepts a feature name, enum name, and its variants as input
+    ($feature:literal, $enum_name:ident { $($variant_name:ident),* }) => {
+        // Conditional derive attributes based on the feature passed
+        #[cfg_attr(feature = $feature, derive(diesel::AsExpression, diesel::FromSqlRow))]
+        #[cfg_attr(feature = $feature, diesel(sql_type = diesel::sql_types::Text))]
+        #[derive(strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
+        #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+        pub enum $enum_name {
+            $(
+                $variant_name,  // Add each variant to the enum
+            )*
+        }
+
+        // Implement common traits for the enum
+        medullah_web::impl_enum_common_traits!($enum_name);
+
+        // Optionally, if the database feature is enabled, implement additional database traits
+        #[cfg(feature = $feature)]
+        medullah_web::impl_enum_diesel_traits!($enum_name);
+    };
+}
