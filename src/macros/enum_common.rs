32367db59dcb958802ffb02cedc5187b68c129da
ahmard <ahmard.mu@gmail.com>
2024-11-09 12:16:10 +0100

    fix(macros): avoid explicit imports

--- src/macros/enum_common.rs
+++ src/macros/enum_common.rs
@@ -1,64 +1,61 @@
 #[macro_export]
+/// Implements Display, Debug, AsRef<str>, serde::Serialize, serde::Deserialize traits for an enum.
 macro_rules! impl_enum_common_traits {
-    ($variant_name:ident, $visitor_name:ident) => {
-        use serde::de::{Unexpected, Visitor};
-        use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
-        use std::fmt;
-        use std::fmt::{Debug, Display, Formatter};
-        use std::str::FromStr;
-
-        impl Display for $variant_name {
-            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-                f.write_str(self.as_str())
+    ($variant_name:ident) => {
+        impl std::fmt::Debug for $variant_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.write_str(&self.to_string())
             }
         }
 
-        impl AsRef<str> for $variant_name {
-            fn as_ref(&self) -> &str {
-                self.as_str()
-            }
-        }
-
-        impl Debug for $variant_name {
-            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-                f.write_str(self.as_str())
-            }
-        }
-
-        impl Serialize for $variant_name {
+        impl serde::Serialize for $variant_name {
             fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
             where
-                S: Serializer,
+                S: serde::Serializer,
             {
-                serializer.serialize_str(self.as_str())
+                serializer.serialize_str(&self.to_string())
             }
         }
 
         // Implement Deserialize
-        impl<'de> Deserialize<'de> for $variant_name {
+        impl<'de> serde::Deserialize<'de> for $variant_name {
             fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
             where
-                D: Deserializer<'de>,
+                D: serde::Deserializer<'de>,
             {
-                struct $visitor_name;
+                struct EnumVisitor;
 
-                impl<'de> Visitor<'de> for $visitor_name {
+                impl<'de> serde::de::Visitor<'de> for EnumVisitor {
                     type Value = $variant_name;
 
-                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
                         formatter.write_str("a valid app event string")
                     }
 
                     fn visit_str<E>(self, value: &str) -> Result<$variant_name, E>
                     where
-                        E: de::Error,
+                        E: serde::de::Error,
                     {
+                        use std::str::FromStr;
+
                         $variant_name::from_str(value)
-                            .map_err(|_| de::Error::invalid_value(Unexpected::Str(value), &self))
+                            .map_err(|_| serde::de::Error::invalid_value(serde::de::Unexpected::Str(value), &self))
                     }
                 }
 
-                deserializer.deserialize_str($visitor_name)
+                deserializer.deserialize_str(EnumVisitor)
+            }
+        }
+    };
+}
+
+#[macro_export]
+/// Implements Display, Debug, AsRef<str>, serde::Serialize, serde::Deserialize traits for an enum.
+macro_rules! impl_enum_display_trait {
+    ($variant_name:ident) => {
+        impl std::fmt::Display for $variant_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.write_str(self.as_str())
             }
         }
     };
--- src/macros/enum_diesel.rs
+++ src/macros/enum_diesel.rs
@@ -1,17 +1,13 @@
 #[macro_export]
+/// Implements Diesel's FromSql and ToSql traits for the given enum.
+/// <br/>**Note:** FromStr, and AsStr traits must be implemented for the given enum
 macro_rules! impl_enum_diesel_traits {
     ($variant_name:ident) => {
-        use diesel::deserialize::FromSql;
-        use diesel::pg::{Pg, PgValue};
-        use diesel::serialize::{IsNull, Output, ToSql};
-        use diesel::sql_types::Text;
+        impl diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg> for $variant_name {
+            fn from_sql(bytes: diesel::pg::PgValue) -> diesel::deserialize::Result<Self> {
+                use std::str::FromStr;
 
-        use std::io::Write;
-
-
-        impl FromSql<Text, Pg> for $variant_name {
-            fn from_sql(bytes: PgValue) -> diesel::deserialize::Result<Self> {
-                let value = <String as FromSql<Text, Pg>>::from_sql(bytes)?;
+                let value = <String as diesel::deserialize::FromSql<diesel::sql_types::Text, diesel::pg::Pg>>::from_sql(bytes)?;
                 match $variant_name::from_str(value.as_str()) {
                     Ok(variant) => Ok(variant),
                     Err(err) => Err(err.to_string().into())
@@ -19,11 +15,13 @@ macro_rules! impl_enum_diesel_traits {
             }
         }
 
-        impl ToSql<Text, Pg> for $variant_name {
-            fn to_sql(&self, out: &mut Output<Pg>) -> diesel::serialize::Result {
-                let value = self.as_str();
+        impl diesel::serialize::ToSql<diesel::sql_types::Text, diesel::pg::Pg> for $variant_name {
+            fn to_sql(&self, out: &mut diesel::serialize::Output<diesel::pg::Pg>) -> diesel::serialize::Result {
+                use std::io::Write;
+
+                let value = self.to_string();
                 out.write_all(value.as_bytes())?;
-                Ok(IsNull::No)
+                Ok(diesel::serialize::IsNull::No)
             }
         }
 
