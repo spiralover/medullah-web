2461e4a9c9243ead840a0aae5b77a0d518459edc
ahmard <ahmard.mu@gmail.com>
2024-11-13 18:03:08 +0100

    refactor(enum): use "SCREAMING_SNAKE_CASE" instead of "snake_case"

--- src/macros/enum_generate.rs
+++ src/macros/enum_generate.rs
@@ -4,7 +4,7 @@ macro_rules! generate_enum {
     ($enum_name:ident { $($variant_name:ident),* }) => {
         // Conditional derive attributes for the database feature
         #[derive(strum_macros::EnumString, strum_macros::Display, Clone, Eq, PartialEq)]
-        #[strum(serialize_all = "snake_case")]
+        #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
         pub enum $enum_name {
             $(
                 $variant_name,  // Add each variant to the enum
